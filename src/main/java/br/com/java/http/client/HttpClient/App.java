4827118403d4cfb4e6a8c51db767d1a8835bcbb5
Bianca <bianca.basso@fatec.sp.gov.br>
2017-03-31 08:55:27 -0300

    a classe básica

---
+++ src/main/java/br/com/java/http/client/HttpClient/App.java
@@ -0,0 +1,13 @@
+package br.com.java.http.client.HttpClient;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
