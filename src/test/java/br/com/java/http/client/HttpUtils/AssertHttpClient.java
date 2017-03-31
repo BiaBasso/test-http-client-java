5323386872a6eb7ce9440d8a82fcb241b93f7e20
Bianca <bianca.basso@fatec.sp.gov.br>
2017-03-31 12:44:24 -0300

    adicionando a classe que valida se é true ou false

---
+++ src/test/java/br/com/java/http/client/HttpUtils/AssertHttpClient.java
@@ -0,0 +1,10 @@
+package br.com.java.http.client.HttpUtils;
+
+import org.junit.Assert;
+
+public class AssertHttpClient {
+
+	public static void assertTrue(boolean value) {
+		Assert.assertTrue(value);
+	}
+}
